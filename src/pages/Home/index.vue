c61f43e07a7207cc5e12b4c613c4f5a19e6aeb1f
HendryV <hendry@dihardjasoftware.com>
2024-09-18 15:19:33 +0700

    select css

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -22,7 +22,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="p-[16px] bg-white dark:bg-[#101418] h-full">
+  <div class="p-[16px] bg-white dark:bg-[#101418] h-full overflow-hidden">
     <!-- <div class="pt-[100px]">
       <CdxLabel class="mb-[12px] text-[18px] p-0 text-black dark:text-white">
         asdasd</CdxLabel
