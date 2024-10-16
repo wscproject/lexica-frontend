080082cf1fb12c705f63bb174c91eeb3fe728aaf
HendryV <hendry@dihardjasoftware.com>
2024-10-16 23:49:48 +0700

    WIP

--- src/components/pages/Session/Card/script-writing/index.vue
+++ src/components/pages/Session/Card/script-writing/index.vue
@@ -3,12 +3,12 @@ import {
   CdxLabel,
   CdxIcon,
   CdxButton,
-  CdxSearchInput,
+  CdxTextArea,
   CdxProgressBar,
   CdxThumbnail,
 } from "@wikimedia/codex";
 import { cdxIconInfoFilled, cdxIconLogoWikidata } from "@wikimedia/codex-icons";
-import { computed, ref } from "vue";
+import { computed, ref, watch } from "vue";
 import debounce from "lodash.debounce";
 import { useI18n } from "vue-i18n";
 import noData from "@/assets/endofresult.svg";
@@ -18,6 +18,8 @@ import { useStore } from "vuex";
 const vuex = useStore();
 
 const { t } = useI18n({ useScope: "global" });
+
+const refText = ref(null);
 const isThemeDark = computed(() => vuex.getters["profile/isDark"]);
 
 const isInfo = ref(false);
@@ -66,6 +68,10 @@ const onInput = debounce(() => {
     emit("setSearch", search.value);
   }
 }, 500);
+
+watch(refText, () => {
+  console.log(refText.value);
+});
 </script>
 
 <template>
@@ -83,26 +89,31 @@ const onInput = debounce(() => {
           minWidth: '200px',
         }"
       >
-        <CdxLabel class="text-[18px] pb-0"
-          >{{ props?.data?.lemma }} ({{
-            props?.data?.externalLexemeSenseId
-          }})</CdxLabel
-        >
+        <CdxLabel class="text-[18px] pb-0">{{
+          t("session.scriptWriting.title1")
+        }}</CdxLabel>
         <!-- This is for header Expand animation helper. Sudden change on header's height will screw with the animation, so we need to delay the text changes so the height can adapt  -->
-
-        <p
-          v-if="props?.data?.gloss"
-          class="overflow-hidden text-ellipsis"
-          :style="{
-            whiteSpace: 'nowrap',
-          }"
-        >
-          {{ props.data.gloss }}
-        </p>
-
-        <p v-else class="text-[16px] p-0">
-          <i>{{ t("session.emptyDescription") }}</i>
-        </p>
+      </div>
+    </div>
+    <div
+      class="px-[16px] bg-white pt-[12px] pb-[12px] dark:bg-[#101418] flex align-center gap-x-[16px] justify-between"
+    >
+      <div
+        class="overflow-auto h-[18.3vh] text-white text-[28px] w-full max-w-[380px] flex align-center"
+        lang="de"
+        style="
+          -webkit-hyphens: auto;
+          -moz-hyphens: auto;
+          -ms-hyphens: auto;
+          hyphens: auto;
+          word-wrap: break-word;
+          width: 100%;
+        "
+      >
+        <span :ref="refText">
+          Pseudopseudohypoparathyroidism Pseudopseudohypoparathyroidism
+          hypothetically hypothetically
+        </span>
       </div>
       <div>
         <CdxIcon
@@ -113,244 +124,8 @@ const onInput = debounce(() => {
       </div>
     </div>
     <div
-      class="px-[16px] overflow-auto bg-white h-full pt-[12px] pb-[12px] dark:bg-[#101418]"
-    >
-      <CdxLabel
-        class="text-[16px] dark:text-[#EAECF0]"
-        style="padding-bottom: 16px"
-        >{{ t("session.main.title") }}</CdxLabel
-      >
-      <div class="relative">
-        <CdxSearchInput
-          aria-label="SearchInput default demo"
-          :placeholder="t('session.main.search')"
-          class="pb-[16px] relative"
-          v-model="search"
-          @input="onInput"
-        />
-
-        <div class="progress absolute top-0" v-if="props.searchLoading">
-          <div
-            class="progress-bar progress-bar-info progress-bar-striped active"
-            style="width: 100%"
-          ></div>
-        </div>
-      </div>
-
-      <div v-if="props.recommendedLoading">
-        <div class="w-full max-w-[896px]">
-          <span
-            class="mb-[8px] text-[#54595D] text-[16px] dark:text-[#A2A9B1]"
-            >{{ t("session.recLoading") }}</span
-          >
-          <CdxProgressBar class="w-full mt-[8px]"></CdxProgressBar>
-        </div>
-      </div>
-
-      <div
-        v-if="
-          !isSearch &&
-          props?.recommendation?.length === 0 &&
-          !props.searchLoading &&
-          !props.recommendedLoading
-        "
-      >
-        <p class="text-[16px] text-[#54595D]">
-          <i>{{ t("session.main.emptySuggestion1") }}</i>
-        </p>
-        <p class="text-[16px] text-[#54595D]">
-          <i>{{ t("session.main.emptySuggestion2") }}</i>
-        </p>
-      </div>
-      <div
-        v-if="
-          !isSearch &&
-          props?.recommendation?.length > 0 &&
-          !props.searchLoading &&
-          !props.recommendedLoading
-        "
-      >
-        <p
-          class="text-[16px] font-normal text-[#54595D] dark:text-[#A2A9B1] pb-[8px]"
-          style="padding-bottom: 16px"
-        >
-          {{ t("session.main.recommendation") }}
-        </p>
-
-        <div
-          v-for="(value, index) in props.recommendation"
-          :key="index"
-          :class="[
-            value.id === selectedItem
-              ? 'border-[2px] border-[#3366CC] bg-[#EAF3FF] dark:bg-[#1C2940]'
-              : 'border border-[var(--border-color-base)] ',
-            'rounded-[2px] p-[12px] flex items-center gap-x-2 mb-[8px] cursor-pointer justify-between',
-          ]"
-          @click="selectItem(value.id, value)"
-        >
-          <div class="flex gap-x-[12px]">
-            <!-- <div
-              class="border border-[#C8CCD1] rounded-[2px] overflow-hidden w-[48px] h-[48px] shrink-0"
-            >
-              <img
-                :src="value?.image || placeholder"
-                class="object-cover h-full w-full"
-              />
-            </div> -->
-            <CdxThumbnail
-              :thumbnail="{ url: value?.image }"
-              :placeholder-icon="cdxIconLogoWikidata"
-            />
-
-            <div>
-              <CdxLabel
-                class="text-[16px] pb-[4px] leading-[20px] dark:text-[#EAECF0]"
-                >{{ value?.label }} ({{ value?.id }})</CdxLabel
-              >
-              <div
-                v-if="value?.description"
-                :lang="value?.language"
-                style="hyphens: auto; -moz-hyphens: auto; word-wrap: break-word"
-              >
-                <p
-                  class="text-[16px] font-normal text-[#54595D] dark:text-[#A2A9B1] pb-0 leading-[22px]"
-                >
-                  {{ value?.description }}
-                </p>
-              </div>
-
-              <p
-                v-else
-                class="text-[16px] font-normal text-[#54595D] dark:text-[#A2A9B1] pb-0"
-                style="padding-bottom: 16px"
-              >
-                <i>{{ t("session.emptyDescription") }}</i>
-              </p>
-            </div>
-          </div>
-          <CdxIcon
-            :icon="cdxIconInfoFilled"
-            class="cursor-pointer"
-            @click="emit('gotoSubItemDetail', value)"
-          />
-        </div>
-      </div>
-
-      <div v-else-if="isSearch">
-        <div
-          v-if="
-            props?.recommendation?.length !== 0 &&
-            !props.searchLoading &&
-            !props.recommendedLoading
-          "
-        >
-          <p
-            class="text-[16px] font-normal text-[#54595D] dark:text-[#A2A9B1] pb-[8px]"
-            style="padding-bottom: 16px"
-          >
-            {{ t("session.main.result") }}
-          </p>
-          <div
-            v-for="(value, index) in props?.recommendation"
-            :key="index"
-            :class="[
-              value.id === selectedItem
-                ? 'border-[2px] border-[#3366CC] bg-[#EAF3FF] dark:bg-[#1C2940]'
-                : 'border border-[var(--border-color-base)]',
-              'rounded-[2px] p-[12px] flex items-center gap-x-2 mb-[8px] cursor-pointer justify-between',
-            ]"
-            @click="selectItem(value.id, value)"
-          >
-            <div class="flex gap-x-[12px]">
-              <!-- <div
-                class="border border-[#C8CCD1] rounded-[2px] overflow-hidden w-[48px] h-[48px] shrink-0"
-              >
-                <img
-                  :src="value?.image || placeholder"
-                  class="object-cover h-full w-full"
-                />
-              </div> -->
-              <CdxThumbnail
-                :thumbnail="{ url: value?.image }"
-                :placeholder-icon="cdxIconLogoWikidata"
-              />
-              <div>
-                <CdxLabel class="text-[16px] dark:text-[#EAECF0]"
-                  >{{ value?.label }} ({{ value?.id }})</CdxLabel
-                >
-                <div
-                  :lang="value?.language"
-                  v-if="value?.description"
-                  style="
-                    hyphens: auto;
-                    -moz-hyphens: auto;
-                    word-wrap: break-word;
-                  "
-                >
-                  <p
-                    class="text-[16px] font-normal text-[#54595D] dark:text-[#A2A9B1] pb-0"
-                    style="padding-bottom: 16px"
-                  >
-                    {{ value?.description }}
-                  </p>
-                </div>
-                <p
-                  v-else
-                  class="text-[16px] font-normal text-[#54595D] dark:text-[#A2A9B1] pb-0"
-                  style="padding-bottom: 16px"
-                >
-                  <i>{{ t("session.emptyDescription") }}</i>
-                </p>
-              </div>
-            </div>
-            <CdxIcon
-              :icon="cdxIconInfoFilled"
-              class="cursor-pointer"
-              @click="emit('gotoSubItemDetail', value)"
-            />
-          </div>
-
-          <div class="w-full">
-            <CdxButton
-              v-if="!props.noLoadData"
-              class="w-full h-[34px]"
-              @click="emit('loadMore')"
-              :disabled="props.loadmoreLoading"
-              >{{
-                props.loadmoreLoading
-                  ? t("session.main.loading")
-                  : t("session.main.loadmore")
-              }}</CdxButton
-            >
-
-            <div
-              v-else-if="props.noLoadData"
-              class="flex justify-center flex-col align-center gap-y-[4px] pt-[8px]"
-            >
-              <img :src="isThemeDark ? noDataDark : noData" alt="logo" />
-              <span
-                class="text-[#54595D] dark:text-[#A2A9B1] text-[16px] text-center"
-              >
-                <i>{{ t("session.main.emptyLoad") }}</i>
-              </span>
-            </div>
-          </div>
-        </div>
-
-        <div
-          v-else-if="
-            props?.recommendation?.length === 0 && !props.searchLoading
-          "
-        >
-          <CdxLabel class="text-[16px] text-[#D73333]">{{
-            t("session.main.emptySearch1")
-          }}</CdxLabel>
-          <CdxLabel class="text-[16px] text-[#D73333]">{{
-            t("session.main.emptySearch2")
-          }}</CdxLabel>
-        </div>
-      </div>
-    </div>
+      class="px-[16px] bg-white h-full pt-[12px] pb-[12px] dark:bg-[#101418]"
+    ></div>
     <div
       class="w-full h-66px border-t border-[#A2A9B1] dark:border-[#72777D] p-[16px] flex align-center bg-white gap-x-[12px] rounded-b-[16px] dark:bg-[#101418]"
     >
@@ -377,8 +152,22 @@ const onInput = debounce(() => {
 </template>
 
 <style>
+.heighted {
+  height: 100%;
+}
 .cdx-text-input__input {
   height: 34px;
+}
+
+.script .cdx-text-area__textarea {
+  height: 100%;
+}
+
+.script .cdx-text-area__textarea {
+  resize: none;
+  height: 100%;
+  max-height: 183px;
+  font-size: 28px;
 }
 
 @media (max-height: 600px) {
